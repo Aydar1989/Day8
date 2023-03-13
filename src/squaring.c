bfc54c5400aa2171527c121fa8d91fc5ac080b10
Jane Charlott <janechar@ev-c3.kzn.21-school.ru>
2023-03-13 16:39:42 +0300

    5

--- src/squaring.c
+++ src/squaring.c
@@ -5,8 +5,7 @@ int input(int *a, int *n);
 void output(int *a, int n);
 void squaring(int *a, int n);
 
-int main()
-{
+int main() {
     int n = 0;
     int data[NMAX];
     input(data, &n);
@@ -16,28 +15,19 @@ int main()
     return 0;
 }
 
-int input(int *a, int *n)
-{
-    scanf("%d\n", n); 
-        for(int *p = a; p - a < *n; p++){
-            scanf("%d", p);
-        }
+int input(int *a, int *n) {
+    scanf("%d\n", n);
+    for (int *p = a; p - a < *n; p++) {
+        scanf("%d", p);
+    }
     return 0;
 }
 
-void output(int *a, int n)
-{   
-    for(int *p = a; p - a < n - 1; p++) 
-        printf("%d ", *p);  
-            printf("%d", *(a + n - 1));
-    
-
+void output(int *a, int n) {
+    for (int *p = a; p - a < n - 1; p++) printf("%d ", *p);
+    printf("%d", *(a + n - 1));
 }
 
-void squaring(int *a, int n)
-{
-    for(int *p = a; p - a < n; p++)
-    *p = *p * *p;
+void squaring(int *a, int n) {
+    for (int *p = a; p - a < n; p++) *p = *p * *p;
 }
-
-
